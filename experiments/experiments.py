da5953781e26096e773a99c363406499a0d21ad3
Altansuren Tumurbaatar <altansurentumurbaatar@son-atumurb-mbp.wireless.emory.edu>
2022-12-08 12:03:37 -0500

    Percolation with target draft

--- experiments/experiments.py
+++ experiments/experiments.py
@@ -14,13 +14,14 @@ from eventgraphs import *
 from queries_mimic_extract import *
 from queries_PI import *
 from queries import *
+from percolation import percolation_centrality_with_target
 
 
 # Event graph configuration
 # t_max = [<delta days>, <delta hours>]
 join_rules_LF = {
-    "t_min": timedelta(days=0, hours=1, minutes=0),
-    "t_max": timedelta(days=2, hours=0),
+    "t_min": timedelta(days=0, hours=0, minutes=0),
+    "t_max": timedelta(days=1, hours=0),
     'w_e_max': 0.3,  # maximum event difference
     # default event difference for different types of events
     'w_e_default': 1,
@@ -32,7 +33,7 @@ join_rules_LF = {
 }
 
 join_rules_HF = {
-    't_min': timedelta(days=0, hours=1, minutes=0),
+    't_min': timedelta(days=0, hours=0, minutes=0),
     't_max': timedelta(days=0, hours=2),
     'w_e_max': 0.3,  # maximum event difference
     # default event difference for different types of events
@@ -48,13 +49,13 @@ conf_LF = {
     'max_age': 89,
     'min_age': 15,
     'starttime': '2143-01-01',
-    'endtime': '2143-02-01',
+    'endtime': '2143-01-14',
     'min_missing_percent': 80,
     'vitals_agg': 'daily',
     'vitals_X_mean': False,
     'interventions': False,
     'label': True,
-    'PI_states': {0: 1, 1: 0.8, 2: 0.6, 3: 0.4, 4: 0.2, 5: 0}
+    'PI_states': {0: 0, 1: 0.2, 2: 0.4, 3: 0.6, 4: 0.8, 5: 1}
 }
 conf_HF = {
     'max_hours': 24,
@@ -74,19 +75,21 @@ def eventgraph_mimiciii(event_list, join_rules, conf, file_name):
     patients = get_patient_demography(conn, conf)
     all_events = list()
     n = 0
-    for event_name in PI_EVENT_NAMES:
+    for event_name in PI_EVENTS:
         events = get_unique_PI_events(conn, event_name, conf)
         for i, e in enumerate(events):
             e['i'] = i + n
         n += len(events)
         print(event_name, len(events))
         all_events += events
+    '''
     events = get_events_interventions(conn, conf)
     for i, e in enumerate(events):
         e['i'] = i + n
     n += len(events)
     print('Interventions', len(events))
     all_events += events
+    '''
     if conf['vitals_X_mean']:
         events = get_events_vitals_X_mean(conn, conf)
     else:
@@ -129,19 +132,22 @@ def eventgraph_mimiciii(event_list, join_rules, conf, file_name):
     #        else (e['i'], e['id']) for e in all_events])
     attrs = dict([(e['i'], e['id']) for e in all_events])
     nx.set_node_attributes(G, attrs, 'group')
-    attrs = dict([(e['i'], "\n".join([str(k) + ":" + str(v)
-                 for k, v in e.items()])) for e in all_events])
+    states = np.zeros(n)
+    for e in all_events:
+        states[e['i']] = e['PI_state']
+    PC = percolation_centrality_with_target(G, states=states, weight='weight')
+    nx.set_node_attributes(G, PC, 'size')
+    nx.set_node_attributes(G, PC, 'value')
+    attrs = dict([(e['i'], "\n".join([str(k) + ": " + str(v)
+        for k, v in e.items()]) + "\nPC: " + PC[e[i]]) for e in all_events])
     nx.set_node_attributes(G, attrs, 'title')
-    attrs = nx.betweenness_centrality(G, weight='weight', normalized=True)
-    nx.set_node_attributes(G, attrs, 'size')
-    nx.set_node_attributes(G, attrs, 'value')
+    #attrs = nx.betweenness_centrality(G, weight='weight', normalized=True)
     attrs = dict([(key, {'value': val, 'title': val})
                  for key, val in A.items()])
     nx.set_edge_attributes(G, attrs)
-
     g = Network(
         directed=True,
-        height=1100,
+        height=1000,
         neighborhood_highlight=True,
         select_menu=True)
     g.hrepulsion()
@@ -153,6 +159,51 @@ def eventgraph_mimiciii(event_list, join_rules, conf, file_name):
     # g.show("mimic.html")
     g.save_graph(file_name)
 
+    fig, ax = plt.subplots(figsize=(20, 15))
+    pos = nx.spring_layout(H, k=0.15, seed=4572321)
+    patient_i = {id_: i for i, id_ in enumerate(patients.keys())}
+    node_color = [patient_i[e['id']] for e in enumerate(all_events)]
+    node_size = [v*20000 for v in PC.values()]
+    nx.draw_networkx(
+        H,
+        pos=pos,
+        arrow=True,
+        with_labels=True,
+        node_color=node_color,
+        node_size=node_size,
+        edge_color="gainsboro",
+        alpha=0.4,
+    )
+
+    # Title/legend
+    font = {"color": "k", "fontweight": "bold", "fontsize": 20}
+    ax.set_title("Percolation Centrality", font)
+    # Change font color for legend
+    font["color"] = "r"
+    ax.text(
+        0.80,
+        0.06,
+        "node size = percolation centrality",
+        horizontalalignment="center",
+        transform=ax.transAxes,
+        fontdict=font,
+    )
+
+    # Resize figure for label readibility
+    ax.margins(0.1, 0.05)
+    fig.tight_layout()
+    plt.axis("off")
+    plt.show()
+    PC_sorted = [[k, v, all_events[k]] for k, v in sorted(PC.items(), key=lambda x: x[1], reverse=True)]
+    pprint.pprint(PC_sorted)
+    PC_top = dict()
+    for info in PC_sorted[:40]:
+        if info[2]['type'] not in PC_top:
+            PC_top[infor[2]['type']] = 1
+        else:
+            PC_top[infor[2]['type']] += 1
+
+    pprint.pprint(PC_top)
 
 if __name__ == "__main__":
     fname_keys = [
@@ -177,7 +228,7 @@ if __name__ == "__main__":
                                for k, v in conf_LF.items() if k in fname_keys])
     fname_LF += '_' + '_'.join([k + '-' + str(v)
                                     for k, v in join_rules_LF.items()
-                                    if k in fname_keys]) + 'PI_groups.html'
+                                    if k in fname_keys]) + 'Position_excluded.html'
 
     # eventgraph_mimiciii(LOW_FREQ_EVENTS, join_rules_HF, conf_HF, fname_HF)
     eventgraph_mimiciii(LOW_FREQ_EVENTS, join_rules_LF, conf_LF, fname_LF)
